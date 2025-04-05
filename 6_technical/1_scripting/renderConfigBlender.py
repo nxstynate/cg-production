b87f6ee5badd77ad707911e292cd26a8c52a6291
Nate <nathanael.paul.lewis@gmail.com>
2025-04-05 00:35:09 -0500

    Updated files on 2025-04-05 00:35:09

--- 6_technical/1_scripting/renderConfigBlender.py
+++ 6_technical/1_scripting/renderConfigBlender.py
@@ -2,11 +2,29 @@ import bpy
 
 ENABLED_GPU = "RTX 4080"
 DISABLED_GPUS = ["Quadro T1000"]
+LIGHTPATHS = 8
+WIDTH = 1920
+HEIGHT = 1080
+PERCENT = 100
+SAMPLES = 32
 
-def set_render_options(scene):
+def set_render_options():
     """Sets global render options."""
+    scene = bpy.context.scene
+    paths = bpy.context.scene.cycles
+    paths.max_bounces = LIGHTPATHS
+    paths.diffuse_bounces = LIGHTPATHS
+    paths.glossy_bounces = LIGHTPATHS
+    paths.transparent_max_bounces = LIGHTPATHS
+    paths.transmission_bounces = LIGHTPATHS
+    paths.volume_bounces = LIGHTPATHS
+    paths.use_fast_gi = False
+    scene.render.engine = 'CYCLES'
+    scene.render.resolution_x = WIDTH
+    scene.render.resolution_y = HEIGHT
+    scene.render.resolution_percentage = PERCENT  # 100% = full resolution
     scene.render.use_motion_blur = False
-    scene.cycles.samples = 128
+    scene.cycles.samples = SAMPLES
     scene.view_layers[0].cycles.use_denoising = True
     scene.render.use_placeholder = False
     scene.render.use_overwrite = True
@@ -21,8 +39,7 @@ def configure_gpu_devices(enabled, disabled):
     print(f"Disabled GPU: {[d.name for d in prefs.devices if not d.use]}")
 
 def run():
-    scene = bpy.context.scene
-    set_render_options(scene)
+    set_render_options()
     configure_gpu_devices(enabled=ENABLED_GPU, disabled=DISABLED_GPUS)
 
 run()
